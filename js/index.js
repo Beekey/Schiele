498cb03383d98b5485219e51ac8c725954edfb52
Beekey <bikkidesigns@gmail.com>
2016-07-01 18:15:05 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,5 @@
+  $(document).ready(function() {
+    $("body").addClass("text-center");
+    $("img").addClass("img-rounded img-responsive center-block");
+    $("ul").css("text-align","left");
+   });
